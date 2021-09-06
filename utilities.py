3557e297a50a5d3c8e5e9bff07a773c59d650603
Martiya <martiya.zarea@gmail.com>
2021-09-06 16:58:20 -0400

    After adding comparision, before cleaning the code

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 def saveData(data, name):
     with open('Data_Set/{}.pickle'.format(name), 'wb') as handle:
@@ -9,19 +10,16 @@ def loadData(name):
         data = pickle.load(handle)
     return data
 
-def getReshapedDataSet(dataset, modelType):
+# For original training only -> splits into training, test and validation sets
+def getReshapedDataSet(dataSet, modelType):
     dataSetSize = dataSet.shape[0]
     testSetBenignSize = int(dataSetSize/10)
     validationSetSize = int(dataSetSize/10)
-    trainingSetSize = dataSetSize - testSetBenignSize - validationSetSize
+    config = loadData("config")
+    sequenceLen = config["sequenceLen"]
+    dimensionsCount = config["dimensionsCount"]
     
-    config = {"dataSetSize":dataSetSize,
-              "testSetBenignSize":testSetBenignSize,
-              "validationSetSize":validationSetSize,
-              "trainingSetSize":trainingSetSize,
-              "sequenceLen":sequenceLen,
-              "dimensionsCount":dimensionsCount}
-    saveData(config, "config")    
+    trainingSetSize = dataSetSize - testSetBenignSize - validationSetSize        
     if(modelType == "fullyConnected" or modelType == "PCA"):
         trainingSet = np.reshape(dataSet[0:trainingSetSize,:,:,:], (trainingSetSize,sequenceLen*dimensionsCount), order = 'C')
         testSet = np.reshape(dataSet[trainingSetSize:trainingSetSize+testSetBenignSize,:,:,:], (testSetBenignSize,sequenceLen*dimensionsCount), order = 'C')
@@ -37,3 +35,83 @@ def getReshapedDataSet(dataset, modelType):
         
     return trainingSet, testSet, validationSet
 
+# Reshapes the given dataset without splitting
+def getReshapedTestSet(dataSet, modelType):
+    dataSetSize = dataSet.shape[0]
+    config = loadData("config")        
+    sequenceLen = config["sequenceLen"]
+    dimensionsCount = config["dimensionsCount"]
+    
+    if(modelType == "fullyConnected" or modelType == "PCA"):
+        testSet = np.reshape(dataSet, (dataSetSize,sequenceLen*dimensionsCount), order = 'C')        
+    elif(modelType == "1DConv"):        
+        testSet = np.reshape(dataSet, (dataSetSize,sequenceLen*dimensionsCount,1), order = 'C')
+    elif(modelType == "LSTM"):      
+        testSet = np.reshape(dataSet, (dataSetSize,sequenceLen,dimensionsCount), order = 'C')
+    return testSet
+
+# Reshapes the given dataset without splitting
+def getReshapedAdversarialDataSet(dataSet, advDataSet, modelType):
+    orgTrainingSet, orgTestSet, orgValidationSet = getReshapedDataSet(dataSet, modelType)
+    reshapedAdvDataSet = getReshapedTestSet(advDataSet, modelType)
+    advTrainingSetCount = int(reshapedAdvDataSet.shape[0]/2)    
+    advTrainingSet = reshapedAdvDataSet[0:advTrainingSetCount]
+    advTestSet = reshapedAdvDataSet[advTrainingSetCount:]
+    trainingSet = np.concatenate((orgTrainingSet,advTrainingSet))    
+    advLabels = np.zeros(advTrainingSet.shape) # worked 100%
+    #advLabels = -advTrainingSet
+    labels = np.concatenate((orgTrainingSet,advLabels))   
+    return trainingSet, labels, advTestSet, orgValidationSet
+
+def getConfusionMatrix(sortedMSELabels):    
+    positiveCount = sum(sortedMSELabels[:,1] == 1)
+    TP = sum(sortedMSELabels[0:positiveCount,1] == 1)
+    FP = sum(sortedMSELabels[0:positiveCount,1] == 0)
+    TN = sum(sortedMSELabels[positiveCount:,1] == 0)
+    FN = sum(sortedMSELabels[positiveCount:,1] == 1)
+    return TP, FP, TN, FN
+
+def getConfusionMatrixWithThreshold(sortedMSELabels,threshold):
+    positiveCount = sum(sortedMSELabels[:,1] == 1)
+    TP = sum((sortedMSELabels[:,1] == 1) & (sortedMSELabels[:,0] >= threshold))
+    FP = sum((sortedMSELabels[:,1] == 0) & (sortedMSELabels[:,0] >= threshold))
+    TN = sum((sortedMSELabels[:,1] == 0) & (sortedMSELabels[:,0] < threshold))
+    FN = sum((sortedMSELabels[:,1] == 1) & (sortedMSELabels[:,0] < threshold))
+    return TP, FP, TN, FN
+
+def rankedPrecisionAndRecall(mseLabelsList):  
+    sortedMSELabels = mseLabelsList[mseLabelsList[:,0].argsort()[::-1]]
+    TP, FP, TN, FN = getConfusionMatrix(sortedMSELabels)
+    precision = TP/(TP+FP)
+    recall = TP/(TP+FN)
+    minPositiveMSE = min(sortedMSELabels[sortedMSELabels[:,1] == 1,0]) # 0.010118610983828593
+    maxNegativeMSE = float("NaN")
+    if(len(sortedMSELabels[sortedMSELabels[:,1] == 0,0])):
+        maxNegativeMSE = max(sortedMSELabels[sortedMSELabels[:,1] == 0,0])
+    return precision, recall, minPositiveMSE, maxNegativeMSE
+    
+def rankedPrecisionAndRecallWithThreshold(mseAndLabelsList,threshold):
+    sortedMSELabels = mseAndLabelsList[mseAndLabelsList[:,0].argsort()[::-1]]
+    TP, FP, TN, FN = getConfusionMatrixWithThreshold(sortedMSELabels,threshold)
+    precision = TP/(TP+FP)
+    recall = TP/(TP+FN)
+    return precision, recall
+    
+    
+
+
+# show_curve() is for plotting training and validation losses
+def show_curve(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+def calculateThreshold(minPos, maxNeg):
+    if minPos >= maxNeg:
+        return maxNeg + (minPos - maxNeg)/10
+    else:
+        return minPos
